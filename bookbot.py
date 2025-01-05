660c6bd3a9311dc9076828bb52f051aed190a5ab
loganc2342 <loganc2342@gmail.com>
2025-01-05 11:47:55 -0800

    implement functionality for flags char_type, reverse, and sort

--- bookbot.py
+++ bookbot.py
@@ -6,15 +6,15 @@ def main():
     parser.add_argument(
         "-c", "--char_type",
         help="Specifies which characters to include in the report. Options: alpha [default], alnum, ascii, numeric",
-        nargs=1, default="alpha")
+        nargs=1, default=["alpha"])
     parser.add_argument(
         "-r", "--reverse",
-        help="Displays the results in ascending order.",
-        action="store_true")
+        help="Displays the results in reverse (ascending order if sorting by recurrence count or descending order if sorting by ascii code).",
+        action="store_false")
     parser.add_argument(
         "-s", "--sort",
         help="Specifies how to sort the results. Options: num [default] (by recurrence count), ascii (by ascii code)",
-        nargs=1, default="num")
+        nargs=1, default=["num"])
     parser.add_argument(
         "-t", "--trim",
         help="Removes the header and footer added by Project Gutenberg from the results.",
@@ -23,26 +23,49 @@ def main():
         "book",
         help="The path to a plaintext file. Designed to be a plaintext eBook from Project Gutenberg (gutenberg.org).")
     args = parser.parse_args()
-    book = args.book
 
-    with open(book) as f:
+    char_type_arg, sort_arg = args.char_type[0], args.sort[0]
+
+    try:
+        check_flags(char_type_arg, sort_arg)
+    except Exception as e:
+        print(f"Error: {e}")
+        return 1
+    
+    # default to descending order if sorting by recurrence count, or to ascending order if sorting by ascii code
+    reverse_arg = args.reverse
+    if sort_arg == "ascii":
+        reverse_arg = not reverse_arg
+
+    with open(args.book) as f:
         book_text = f.read()
 
     num_words = word_count(book_text)
     num_chars = character_count(book_text)
     num_chars = dict_convert_to_list(num_chars)
-    num_chars.sort(reverse=True, key=sort_on)
 
-    print(f"--- Begin report of {book} ---")
+    if sort_arg == "ascii":
+        num_chars.sort(reverse=reverse_arg, key=sort_on_ascii)
+    elif sort_arg == "num":
+        num_chars.sort(reverse=reverse_arg, key=sort_on_num)
+
+    print(f"--- Begin report of {args.book} ---")
     print(f"{num_words} words found in the document\n")
-
+    
     for entry in num_chars:
-        if entry["key"].isalpha():
-            print(f"The '{entry["key"]}' character was found {entry["value"]} times")
+        line = report_entry(entry, char_type_arg)
+        if line != None:
+            print(line)
 
     print("--- End report ---")
+
+
+def check_flags(char_type, sort):
+    if not (char_type == "alpha" or char_type == "alnum" or char_type == "ascii" or char_type == "numeric"):
+        raise Exception("invalid argument for flag 'char_type'")
     
-# Flags: --help, --type=alpha (alnum, ascii, numeric), -r (--remove, removes header), --sort=num (ascii)
+    if sort != "num" and sort != "ascii":
+        raise Exception("invalid argument for flag 'sort'")
 
 def word_count(text):
     words = text.split()
@@ -68,8 +91,21 @@ def dict_convert_to_list(dict):
 
     return new_list
 
-def sort_on(dict):
+def sort_on_num(dict):
     return dict["value"]
+
+def sort_on_ascii(dict):
+    return dict["key"]
+
+def report_entry(entry, char_type):
+    if char_type == "alnum" and entry["key"].isalnum():
+        return f"The '{entry["key"]}' character was found {entry["value"]} times"
+    elif char_type == "ascii" and entry["key"].isascii():
+        return f"The '{entry["key"]}' character was found {entry["value"]} times"
+    elif char_type == "numeric" and entry["key"].isnumeric():
+        return f"The '{entry["key"]}' character was found {entry["value"]} times"
+    elif char_type == "alpha" and entry["key"].isalpha():
+        return f"The '{entry["key"]}' character was found {entry["value"]} times"
 
 
 main()
